73d192e93e0381d1d9fe5b6659f73fe1614cf6fb
mobocomputer <mobocomputer@TEST-SYSTEM>
2023-11-11 05:07:40 -0800

    writed base entities.

---
+++ MarketPlace/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketPlace.Entities
+{
+    internal class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int QuantityInStock { get; set; }
+        public DateTime ExpirationDate { get; set; }
+    }
+}
